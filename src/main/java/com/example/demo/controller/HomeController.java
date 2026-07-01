7d0a19ccddd71fba8877808bc072a1a03b4e42d1
UserK <UserK@192.168.219.110>
2026-07-01 15:24:29 +0900

    테스트

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -8,7 +8,7 @@ public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "Spring Boot + Jenkins + GitHub Pipeline Success!";
+        return "Spring Boot + Jenkins + GitHub Pipeline Success! 123";
     }
 
     @GetMapping("/hello")
